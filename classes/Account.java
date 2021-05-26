e896ee117ccc2da447ff6e180bcb32a8182bc661
aaryan Gambhir <aaryangambhir9@gmail.com>
2021-05-26 22:19:12 +0530

    practice classes objects and constructors messed around with some code

---
+++ classes/Account.java
@@ -0,0 +1,83 @@
+public class Account {
+    private String accountNumber;
+    private double balance;
+    private String name;
+    private String email;
+    private String phone;
+
+    public void deposit(double deposit)
+    {
+        balance+=deposit;
+        System.out.println("Deposit Successfull.NEW BALANCE = "+this.balance);
+    }
+
+    public Account(){
+        System.out.println("Empty constructor called");
+    }
+    public Account (String accountNumber, double balance, String name, String email, String phone){
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+    }
+    public void withdraw(double withdraw)
+    {
+        if(balance - withdraw < 0){
+            System.out.println("INSUFFICENT BALANCE. BALANCE AVAILABLE = "+this.balance);
+        }
+        else{
+            balance -= withdraw;
+            System.out.println("Withdrawl of "+ withdraw +" processed. Remaining balance = "+this.balance);
+        }
+    }
+
+    //GETTERS
+    public String GetAccountNumber(){
+        return accountNumber;
+    }
+    public double GetBalance(){
+        return balance;
+    }
+    public String GetName(){
+        return name;
+    }
+    public String GetEmail(){
+        return email;
+    }
+    public String GetPhone(){
+        return phone;
+    }
+
+    //SETTERS
+
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public static void main(String[] args)
+    {
+        Account Aaryan= new Account("12346", 100, "Aaryan Gambhir","crimson@gmail.com","123456789");
+
+        Aaryan.deposit(700);
+        Aaryan.withdraw(400);
+    }
+    
+}
---
+++ classes/sum_calc.java
@@ -0,0 +1,63 @@
+
+import java.util.Scanner;
+
+public class sum_calc{
+    public static void main(String[] args)
+    {
+        sum_calc calculator = new sum_calc();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber(){
+        return firstNumber;
+    }
+
+    public double getSecondNumber(){
+        return secondNumber;
+    }
+
+    public void setFirstNumber(double num)
+    {
+        firstNumber=num;
+    }
+
+    public void setSecondNumber(double num)
+    {
+        secondNumber=num;
+    }
+    public double getAdditionResult()
+    {
+        return getFirstNumber() + getSecondNumber();
+    }
+    public double getSubtractionResult()
+    {
+        return getFirstNumber() - getSecondNumber();
+    }
+    public double getMultiplicationResult()
+    {
+        return getFirstNumber() * getSecondNumber();
+    }
+    public double getDivisionResult ()
+    {
+        if(secondNumber == 0){
+            return 0;
+        }
+        else{
+            return getFirstNumber() / getSecondNumber();
+        }
+    }
+}
+
+/*
+    getAdditionResult
+    getSubtractionResult
+    getMultiplicationResult
+    getDivisionResult 
+*/
